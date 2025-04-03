76bb2632a573378e74f983ea02a77168607091ed
prathyusha1993 <prathyusha9310@gmail.com>
2025-04-03 13:17:26 -0700

    added route for create friend and delete friend and update friend

--- backend_flask_server/routes.py
+++ backend_flask_server/routes.py
@@ -7,3 +7,70 @@ def get_friends():
     friends = Friend.query.all()
     result = [friend.to_json() for friend in friends]
     return jsonify(result)
+
+# create a new friend
+@app.route('/friends', methods=['POST'])
+def create_friend():
+    try:
+        data = request.json
+
+        required_fields = ['name', 'role', 'description', 'gender']
+        for field in required_fields:
+            if field not in data:
+                return jsonify({ 'error': f'Missing field: {field}'}), 400
+            
+        name = data.get('name')
+        role = data.get('role')
+        description = data.get('description')
+        gender = data.get('gender')
+        # fetch avatar image url if it exists for image url
+        if gender == 'male':
+            img_url = f"https://avatar.iran.liara.run/public/boy?username={name}"
+        elif gender == 'female':
+            img_url = f"https://avatar.iran.liara.run/public/girl?username={name}"
+        else:
+            img_url = None
+
+        new_friend = Friend(name=name, role=role, description=description, gender=gender, img_url=img_url)
+        db.session.add(new_friend)
+        db.session.commit()
+        return jsonify({'message': 'New friend created!'}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 500
+
+# delete a friend
+@app.route('/friends/<int:id>', methods=['DELETE'])
+def delete_friend(id):
+    try:
+        friend = Friend.query.get(id)
+        if friend is None:
+            return jsonify({'error': 'Friend not found'}), 404
+        
+        db.session.delete(friend)
+        db.session.commit()
+        return jsonify({'message': 'Friend deleted!'}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 500
+
+
+# update a friend
+@app.route('/friends/<int:id>', methods=['PATCH'])
+def update_friend(id):
+    try:
+        friend = Friend.query.get(id)
+        if friend in None:
+            return jsonify({ 'error': 'Friend not found'}), 404
+        
+        data = request.json
+        friend.name = data.get('name', friend.name)
+        friend.role = data.get('role', friend.role)
+        friend.description = data.get('description', friend.description)
+        friend.gender = data.get('gender', friend.gender)
+
+        db.session.commit()
+        return jsonify(friend.to_json()), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({ 'error': str(e)}), 500
