8a5d251216e15cda70762601318f74ca64c4ba0a
prathyusha1993 <prathyusha9310@gmail.com>
2025-04-07 13:03:42 -0700

    import change in wsgi file

--- backend_flask_server/wsgi.py
+++ backend_flask_server/wsgi.py
@@ -1,7 +1,7 @@
-from app import app
+from main import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 # Gunicorn configuration and WSGI(Web Server Gateway Interface) entry point are both compojnets use din deploying and serving
 # python web applications, particularly those built with frameworks like Flask or Django.
