5aa934fd65553b5b0a3b9edd37211457366903e0
prathyusha1993 <prathyusha9310@gmail.com>
2025-04-07 12:09:38 -0700

    configured main.py for static files for deployment, requirements.txt file

--- backend_flask_server/main.py
+++ backend_flask_server/main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
@@ -11,6 +12,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+frontend_folder = os.path.join(os.getcwd(), '..', 'chakra_react_frontend')
+dist_folder = os.path.join(frontend_folder, 'dist')
+
+# serve static files from the 'dist' folder under the 'chakra react frontend' directory
+@app.route('/', defaults={'filename': ''})
+@app.route('/<path:filename>')
+def index(filename):
+    if not filename:
+        filename = 'index.html'
+    return send_from_directory(dist_folder, filename)
+
+
+# api routes
 import routes
 
 with app.app_context():
